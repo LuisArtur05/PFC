1734ad803dd014c90acab33601e1cb3347ee3bfb
Misaelvilla <villagomezmisael@gmail.com>
2025-04-08 13:40:41 +0200

    Migración a nuevos directorios backend y frontend

---
+++ frontend-react/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+// src/pages/Dashboard.jsx
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <p>This is the dashboard page.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
