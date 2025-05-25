5acd70836ffd1f61d7ac1149fa9667d3e4087fd9
Junior028 <luis028rp@gmail.com>
2025-05-25 23:04:55 +0200

    ejecutable

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+Write-Host "Iniciando backend Spring Boot..."
+Start-Process -NoNewWindow -FilePath "mvn" -ArgumentList "-f NeveraApi/pom.xml spring-boot:run"
+
+Start-Sleep -Seconds 10
+
+Write-Host "Iniciando frontend React..."
+Set-Location -Path "frontend-react"
+npm install
+npm start
